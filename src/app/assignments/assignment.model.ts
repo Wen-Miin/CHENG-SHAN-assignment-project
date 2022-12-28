204d975379da0691b832a1d4233be43cb7d7dae7
wen <71599550+Wen-Miin@users.noreply.github.com>
2022-12-28 02:44:26 +0100

    ajout des propriétés pour un assignment

--- src/app/assignments/assignment.model.ts
+++ src/app/assignments/assignment.model.ts
@@ -1,7 +1,12 @@
 export class Assignment {
   _id?:string;
   id!:number;
-  nom!: string;
+  nom!: string; // nom du devoir
+  auteur!: string;  // nom de l'étudiant
+  matiere!: string; // nom de la matière
+  note!: number; // note sur 20
   dateDeRendu!: Date;
-  rendu!: boolean;
+  rendu!: boolean; // true si le devoir a été rendu
+  remarque!: string; // remarque facultative
+  // TODO : ajouter une propriété image
 }
